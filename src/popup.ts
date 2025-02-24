1406f58b87f5a86ae9906f1840d30758d0f0bbcf
kurakurakuda <kris.3618pcp@gmail.com>
2025-02-24 22:12:53 +0900

    #9 modify the input filed to support text and dropdown

--- src/popup.ts
+++ src/popup.ts
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log('Popup loaded.');
 
     const gssUrlInput = document.querySelector<HTMLInputElement>('#gss-url input');
+    const gssUrlSelectBtn = document.querySelector<HTMLButtonElement>('#gss-url button');
+    const gssUrlDropdown = document.querySelector<HTMLSelectElement>('#gss-url select');
+
     const titleInput = document.querySelector<HTMLInputElement>('#title input');
+
     const categoryInput = document.querySelector<HTMLInputElement>('#category input');
+    const categorySelectBtn = document.querySelector<HTMLButtonElement>('#category button');
+    const categoryDropdown = document.querySelector<HTMLSelectElement>('#category select');
+    
     const saveBtn = document.querySelector('#save');
     const cancelBtn = document.querySelector('#cancel');
 
@@ -13,6 +20,38 @@ document.addEventListener('DOMContentLoaded', async () => {
     if (titleInput && title) {
       titleInput.value = title;
     }
+
+    gssUrlSelectBtn?.addEventListener('click', function() {
+      // TODO リファクタ候補
+      if (gssUrlDropdown) {
+        gssUrlDropdown.style.display = (gssUrlDropdown.style.display === 'block') ? 'none' : 'block';
+        gssUrlDropdown.selectedIndex = -1;
+      }
+    })
+
+    gssUrlDropdown?.addEventListener('change', function() {
+      // TODO リファクタ候補
+      if (gssUrlInput) {
+        gssUrlInput.value = gssUrlDropdown.value;
+        gssUrlDropdown.style.display ='none';
+      }
+    })
+
+    categorySelectBtn?.addEventListener('click', function() {
+      // TODO リファクタ候補
+      if (categoryDropdown) {
+        categoryDropdown.style.display = (categoryDropdown.style.display === 'block') ? 'none' : 'block';
+        categoryDropdown.selectedIndex = -1;
+      }
+    })
+
+    categoryDropdown?.addEventListener('change', function() {
+      // TODO リファクタ候補
+      if (categoryInput) {
+        categoryInput.value = categoryDropdown.value;
+        categoryDropdown.style.display ='none';
+      }
+    })
 
     saveBtn?.addEventListener('click', async function() {
       const gssUrl = gssUrlInput?.value;
@@ -36,12 +75,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 
       const data = [title, link, category, formattedToday];
 
-      console.log(gssId, data);
-
       try {
         const token = await getAccessToken();
-        console.log(token);
-
         const response = await fetch(
           `https://sheets.googleapis.com/v4/spreadsheets/${gssId}/values/A:D:append?valueInputOption=RAW`,
           {
@@ -69,11 +104,24 @@ document.addEventListener('DOMContentLoaded', async () => {
       window.close();
     });
 
-  // キャンセルボタンのクリックイベント
-  cancelBtn?.addEventListener('click', function() {
-      window.close();
-  });
+    // キャンセルボタンのクリックイベント
+    cancelBtn?.addEventListener('click', function() {
+        window.close();
+    });
 });
+
+document.addEventListener('click', function(event) {
+  const gssUrlArea = document.querySelector<HTMLElement>('#gss-url');
+  const gssUrlDropdown = document.querySelector<HTMLSelectElement>('#gss-url select');
+  const categoryArea = document.querySelector<HTMLElement>('#category');
+  const categoryDropdown = document.querySelector<HTMLSelectElement>('#category select');
+  if (!gssUrlArea?.contains(event.target as Node) && gssUrlDropdown) {
+    gssUrlDropdown.style.display = 'none';
+  }
+  if (!categoryArea?.contains(event.target as Node) && categoryDropdown) {
+    categoryDropdown.style.display = 'none';
+  } 
+})
 
 async function getAccessToken() {
   return new Promise((resolve, reject) => {
